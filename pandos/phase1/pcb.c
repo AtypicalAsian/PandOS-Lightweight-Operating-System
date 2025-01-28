97156b5b6bf8224461836173cd6aec83c766a0cc
Nicolas Nguyen <quangnguyen2003006@gmail.com>
2025-01-27 22:09:00 -0500

    fix commenting to abide by C convetions & finish outProcQ implementation

--- pandos/phase1/pcb.c
+++ pandos/phase1/pcb.c
@@ -19,9 +19,10 @@ HIDDEN pcb_PTR pcbFree_h;
  *  return: none 
  *****************************************************************************/
 void initPcbs() {
-	static pcb_t pcb_pool[MAXPROC];         //Pool of PCBs from which processes can be allocated
-	pcbFree_h = NULL;                       //List is initially empty
-	for(int i = 0; i < MAXPROC; i++){
+	static pcb_t pcb_pool[MAXPROC];         /*Pool of PCBs from which processes can be allocated*/
+	pcbFree_h = NULL;                       /*List is initially empty*/
+    int i;
+	for(i = 0; i < MAXPROC; i++){
 		//add each pcb to pcbFree list
 		freePcb(&(pcb_pool[i]));
 	}
@@ -35,12 +36,12 @@ void initPcbs() {
  *  return: none 
  *****************************************************************************/
 void freePcb(pcb_PTR p){
-    if (pcbFree_h == NULL){                //If pcbFree list is empty
+    if (pcbFree_h == NULL){                /*If pcbFree list is empty*/
 		(*p).p_prev = NULL;
         (*p).p_next = NULL;
 		pcbFree_h = p;
 	}
-	else{                                 //Otherwise, if there are items in pcbFree list
+	else{                                 /*Otherwise, if there are items in pcbFree list*/
         (*p).p_prev = NULL;
 		(*p).p_next = pcbFree_h;
 		(*pcbFree_h).p_prev = p;
@@ -57,38 +58,39 @@ void freePcb(pcb_PTR p){
 
 pcb_PTR allocPcb(){
    if (pcbFree_h == NULL) return NULL;
-   pcb_PTR freed_pcb_ptr = pcbFree_h;           //remove from head of linked list
+   pcb_PTR freed_pcb_ptr = pcbFree_h;           /*remove from head of linked list*/
 
-   //if pcbFree only has one free pcb left
+   /*if pcbFree only has one free pcb left*/
    if ((*freed_pcb_ptr).p_next == NULL) pcbFree_h = NULL;
 
    else{
-    //detach next and prev pointers + update new head of freePcb linked list
+    /*detach next and prev pointers + update new head of freePcb linked list*/
     ((*freed_pcb_ptr).p_next)->p_prev = NULL;
     (*freed_pcb_ptr).p_prev = NULL;
     pcbFree_h = (*freed_pcb_ptr).p_next;
    }
 
-   //intialize values for all pcb struct fields of newly freed pcb 
+   /*intialize values for all pcb struct fields of newly freed pcb*/
    
-   //Process queue fields
+   /*Process queue fields*/
    (*freed_pcb_ptr).p_prev = NULL;
    (*freed_pcb_ptr).p_next = NULL;
 
-   //Process tree fields
+   /*Process tree fields*/
    (*freed_pcb_ptr).p_child = NULL;
    (*freed_pcb_ptr).p_sib = NULL;
    (*freed_pcb_ptr).p_prnt = NULL;
 
-   //Process status info
-   for (int i = 0; i < STATEREGNUM; i++){
+   /*Process status info*/
+   int i;
+   for (i = 0; i < STATEREGNUM; i++){
 		freed_pcb_ptr->p_s.s_reg[i] = 0;
 	}
    (*freed_pcb_ptr).p_time = 0;
    (*freed_pcb_ptr).p_semAdd = NULL;
 
-   //Support layer info
-   //p_supportStruct = NULL; ???? HAVE NOT DECLARED THIS AND UNSURE ABOUT INIT VALUE */
+   /*Support layer info*/
+   /*p_supportStruct = NULL; ???? HAVE NOT DECLARED THIS AND UNSURE ABOUT INIT VALUE */
 
     return freed_pcb_ptr;
 }
@@ -131,16 +133,16 @@ int emptyProcQ (pcb_PTR tp){
  *  return: none
  *****************************************************************************/
 void insertProcQ (pcb_PTR *tp, pcb_PTR p){
-    //If process queue empty --> new pcb becomes single node in circular DLL
+    /*If process queue empty --> new pcb becomes single node in circular DLL*/
     if (emptyProcQ(*tp)){
-        *tp = p;            //p is new tail pointer
-        p->p_next = p;      //update next and prev to point to itself
+        *tp = p;            /*p is new tail pointer*/
+        p->p_next = p;      /*update next and prev to point to itself*/
         p->p_prev = p;
     }
 
     else{
-        //p becomes new tail, update current tail and associated next, prev pointers
-        pcb_PTR currHead = (*tp)->p_next;       //current head of circular DLL
+        /*p becomes new tail, update current tail and associated next, prev pointers*/
+        pcb_PTR currHead = (*tp)->p_next;       /*current head of circular DLL*/
         p->p_next = currHead;
         p->p_prev = *tp;
         currHead->p_prev = p;
@@ -161,7 +163,7 @@ pcb_PTR removeProcQ (pcb_PTR *tp){
     if (emptyProcQ(*tp)) return NULL;
     else{
         pcb_PTR removedHead = (*tp)->p_next;
-        //If queue only has 1 pcb
+        /*If queue only has 1 pcb*/
         if(removedHead == *tp) *tp = NULL;
         else{
             (*tp)->p_next = removedHead->p_next;
@@ -182,15 +184,25 @@ pcb_PTR removeProcQ (pcb_PTR *tp){
 pcb_PTR outProcQ (pcb_PTR *tp, pcb_PTR p){
     if (emptyProcQ(*tp)) return NULL;
 
-    pcb_PTR curr = (*tp)->p_next;           //start at head
-    pcb_PTR prev = *tp;                     //trailing pointer starting from tail
+    pcb_PTR curr = (*tp)->p_next;           /*start at head*/
+    pcb_PTR prev = *tp;                     /*trailing pointer starting from tail*/
 
 
-    //Loop through the list with curr,prev pointers until we find p, or until we circle back to the head (complete one loop)
+    /*Loop through the list with curr,prev pointers until we find p, or until we circle back to the head (complete one loop)*/
     do {
         if (curr == p){
-            //Disconnect node from queue + update associated pointers
-            (...)
+            /*Disconnect node from queue*/
+            prev->p_next = curr->p_next;
+            curr->p_next->p_prev = prev;
+
+            //if curr is the tail
+            if (curr == *tp){
+                *tp = (curr == prev) ? NULL : prev;
+            }
+
+            curr->p_next = NULL;
+            curr->p_prev = NULL;
+            return curr;
         }
         prev = curr;
         curr = curr->p_next;

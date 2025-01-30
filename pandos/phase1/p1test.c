8e4c2965fc6221887125c29d3ac45b9107b2190c
kaia15 <baotranongtran@gmail.com>
2025-01-30 13:15:59 -0500

    add functions to test insertBlocked & removeBlocked

--- pandos/phase1/p1test.c
+++ pandos/phase1/p1test.c
@@ -247,5 +247,43 @@ void main() {
 	for (i = 0; i < 10; i++) 
 		freePcb(procp[i]);
 
+	initASL();
+	addokbuf("Initialized active semaphore list   \n");
+
+	/* check removeBlocked and insertBlocked */
+	addokbuf("insertBlocked test #1 started  \n");
+	for (i = 10; i < MAXPROC; i++) {
+		procp[i] = allocPcb();
+		if (insertBlocked(&sem[i], procp[i]))
+			adderrbuf("insertBlocked(1): unexpected TRUE   ");
+	}
+	addokbuf("insertBlocked test #2 started  \n");
+	for (i = 0; i < 10; i++) {
+		procp[i] = allocPcb();
+		if (insertBlocked(&sem[i], procp[i]))
+			adderrbuf("insertBlocked(2): unexpected TRUE   ");
+	}
+
+	/* check if semaphore descriptors are returned to free list */
+	p = removeBlocked(&sem[11]);
+	if (insertBlocked(&sem[11],p))
+		adderrbuf("removeBlocked: fails to return to free list   ");
+
+	if (insertBlocked(&onesem, procp[9]) == FALSE)
+		adderrbuf("insertBlocked: inserted more than MAXPROC   ");
+	
+	addokbuf("removeBlocked test started   \n");
+	for (i = 10; i< MAXPROC; i++) {
+		q = removeBlocked(&sem[i]);
+		if (q == NULL)
+			adderrbuf("removeBlocked: wouldn't remove   ");
+		if (q != procp[i])
+			adderrbuf("removeBlocked: removed wrong element   ");
+		if (insertBlocked(&sem[i-10], q))
+			adderrbuf("insertBlocked(3): unexpected TRUE   ");
+	}
+	if (removeBlocked(&sem[11]) != NULL)
+		adderrbuf("removeBlocked: removed nonexistent blocked proc   ");
+	addokbuf("insertBlocked and removeBlocked ok   \n);
 }
 

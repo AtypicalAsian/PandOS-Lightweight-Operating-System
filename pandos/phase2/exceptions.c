4c5f30c40d5cc8b04cde7131c2e3f68a5fe03048
Nicolas Nguyen <quangnguyen2003006@gmail.com>
2025-02-10 00:15:21 -0500

    initialize exceptions and scheduler modules

--- pandos/phase2/exceptions.c
+++ pandos/phase2/exceptions.c
@@ -8,3 +8,21 @@ This module contains the implementation of the exception handler
 To view version history and changes:
     - Remote GitHub Repo: https://github.com/AtypicalAsian/CS372-OS-Project
 ****************************************************************************/
+
+#include "../h/asl.h"
+#include "../h/types.h"
+#include "../h/const.h"
+#include "../h/pcb.h"
+#include "/usr/include/umps3/umps/libumps.h"
+
+
+/*function prototypes*/
+HIDDEN void blockCurr(int *sem);
+HIDDEN void createProcess(state_PTR stateSYS, support_t *suppStruct);
+HIDDEN void terminateProcess(pcb_PTR proc);
+HIDDEN void waitOp(int *sem);
+HIDDEN void signalOp(int *sem);
+HIDDEN void waitForIO(int lineNum, int deviceNum, int readBool);
+HIDDEN void getCPUTime();
+HIDDEN void waitForPClock();
+HIDDEN void getSupportData();
--- pandos/phase2/scheduler.c
+++ pandos/phase2/scheduler.c
@@ -9,3 +9,10 @@ scheduler.
 To view version history and changes:
     - Remote GitHub Repo: https://github.com/AtypicalAsian/CS372-OS-Project
 ****************************************************************************/
+
+#include "../h/asl.h"
+#include "../h/types.h"
+#include "../h/const.h"
+#include "../h/pcb.h"
+#include "/usr/include/umps3/umps/libumps.h"
+

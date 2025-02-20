3158eee9f1d521462422541e4d27721649450af3
kaia15 <baotranongtran@gmail.com>
2025-02-20 17:10:45 -0500

    question about input of setSTATUS

--- pandos/phase2/scheduler.c
+++ pandos/phase2/scheduler.c
@@ -43,7 +43,7 @@ void switchProcess() {
             HALT();
         } else if (softBlockCnt > 0) {
             /* There are blocked processes, enter Wait State */
-            setSTATUS(ENABLE_INTERRUPTS | DISABLE_PLT);  /* Enable interrupts, disable PLT */
+            setSTATUS(ENABLE_INTERRUPTS | DISABLE_PLT);  /* getSTATUS first, then change its status => invoke BITs? */
             WAIT();
         } else {
             /* Deadlock condition -> PANIC */

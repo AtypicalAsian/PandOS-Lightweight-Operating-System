2def9b4c8c93fe50e7bbbd1b639caadcd1c76fac
Nicolas Nguyen <quangnguyen2003006@gmail.com>
2025-02-20 23:25:21 -0500

    mod scheduler.c

--- pandos/phase2/scheduler.c
+++ pandos/phase2/scheduler.c
@@ -3,8 +3,16 @@ CS372 - Operating Systems
 Dr. Mikey Goldweber
 Written by: Nicolas & Tran
 
-This module contains the implementation of the deadlock detector and the 
-scheduler.
+This module implements process scheduling and deadlock detection to ensure the 
+system maintains progress and prevents indefinite waiting. It employs a preemptive 
+round-robin scheduling algorithm with a five-millisecond time slice, ensuring that 
+each process in the Ready Queue gets a fair share of CPU time. 
+
+If a process is available, it is removed from the Ready Queue, assigned to the Current Process, 
+and executed. If no ready processes exist, the system evaluates different conditions: 
+if no processes remain, it halts execution; if processes are blocked on I/O, the 
+system enters a wait state; otherwise, if processes exist but are stuck (deadlock), 
+the system triggers a panic.
 
 To view version history and changes:
     - Remote GitHub Repo: https://github.com/AtypicalAsian/CS372-OS-Project

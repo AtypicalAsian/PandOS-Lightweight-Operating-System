f11a7f25775c3f81931d529fe1fbe359e5a795c4
kaia15 <baotranongtran@gmail.com>
2025-02-25 20:20:31 -0500

    add Big Picture Comments to exceptions.ml

--- pandos/phase2/exceptions.c
+++ pandos/phase2/exceptions.c
@@ -20,33 +20,6 @@ To view version history and changes:
 #include "../h/interrupts.h"
 #include "../h/initial.h"
 
-
-/* 
-BIG PICTURE: 
-There are 4 main types of exception handler: 
-
-(??? implement in interrupts.c)
-• For exception code 0 (Interrupts), processing should be passed along to your
-Nucleus’s device interrupt handler. [Section 3.6] 
-
-(already written in pandos.pdf)
-• For exception codes 1-3 (TLB exceptions), processing should be passed
-along to your Nucleus’s TLB exception handler. [Section 3.7.3]
-
-
-• For exception codes 4-7, 9-12 (Program Traps), processing should be passed
-along to your Nucleus’s Program Trap exception handler. [Section 3.7.2]
-
-(
-Instructions in pandos, focus on state_t defined in types.h to get register array and read address from this array
-Set Processor 0 address as 0x0FFF.F000 (BIOS Datapage)
-)
-• For exception code 8 (SYSCALL), processing should be passed along to
-your Nucleus’s SYSCALL exception handler. [Section 3.5]
-
-*/
-
-
 /*function prototypes*/
 HIDDEN void blockCurr(int *sem);
 /* HIDDEN void createProcess(state_PTR stateSYS, support_t *suppStruct); */ 
@@ -59,73 +32,98 @@ HIDDEN void waitForPClock();
 HIDDEN void getSupportData();
 
 void createProcess(state_PTR stateSYS, support_t *suppStruct) {
-    pcb_PTR newProc = allocPcb(); /* Allocate a new PCB */ 
+
+    /* BIG PICTURE:
+    - Allocate a new Process Control Block (PCB) for the new process.
+    - If no PCB is available, return an error (-1) in the caller's v0 register.
+    - Initialize the PCB fields, including the process state and support structure.
+    - Reset CPU time usage and set the process as ready (not blocked on a semaphore).
+    - Insert the new process into the process tree as a child of the current process.
+    - Add the new process to the Ready Queue for scheduling.
+    - Increment the process count to track active processes.
+    - Return success (0) in the caller's v0 register.
+    */
+    pcb_PTR newProc = allocPcb(); 
     if (newProc == NULL) {
-        /* No available PCB, return error (-1) in v0 of the caller */
         currProc->p_s.s_v0 = -1;
         return;
     }
     
-    /* 
-    Steps: 
-        1. Initialize PCB fields 
-        2. Set initial processor state
-        3. Assign support structure (if any)
-        4. Reset CPU time
-        5. Set semaphore to NULL (ready state)
-    */ 
     newProc->p_s = *stateSYS; 
     newProc->p_supportStruct = suppStruct; 
     newProc->p_time = 0; 
     newProc->p_semAdd = NULL; 
-    
-    /* Insert into process tree and ready queue */ 
+     
     insertChild(currProc, newProc); 
     insertProcQ(&ReadyQueue, newProc); 
     
-    /* Increment process count */ 
     procCnt++;
-    
-    /* Return success (0) in v0 of the caller */ 
+ 
     currProc->p_s.s_v0 = 0;
 }
 
 void terminateProcess(pcb_PTR proc) {
     pcb_PTR childProc;
 
-    /* Terminate all child processes*/ 
+    /* BIG PICTURE:
+    - Recursively terminate all child processes before terminating the current process.
+    - There are 2 cases: 
+        - If the process is blocked on a semaphore, remove it from the Active Semaphore List (ASL).
+        - If the process is in the Ready Queue, remove it.
+    - Remove the process from its parent's child list.
+    - Free the Process Control Block (PCB) to make it available for future processes.
+    - If the terminated process is the currently running process, reset `currProc` and switch to a new process using `Scheduler()`.
+    */
+ 
     while ((childProc = removeChild(proc)) != NULL) {
         terminateProcess(childProc);
     }
-
-    /* Case 1: If the process is blocked on a semaphore, remove it from ASL */ 
+    
     if (proc->p_semAdd != NULL) {
-        outBlocked(proc);  // Remove from the Active Semaphore List
+        outBlocked(proc);  
     }
-
-    /* Case 2: If the process is in the Ready Queue, remove it */ 
+    
     outProcQ(&ReadyQueue, proc);
 
-    /* Remove from the parent's child list */ 
     if (proc->p_parent != NULL) {
         outChild(proc);
     }
 
-    /* Free the PCB */
     freePcb(proc);
 
-    /* If the terminated process is the current one, call the scheduler */ 
     if (proc == currProc) {
         currProc = NULL;
-        switchProcess();  /* Switch to a new process using Scheduler() for context switch */
+        switchProcess(); 
     }
 }
 
 void waitOp(int *sem) {
     (*sem)--;  /* Decrement the semaphore */ 
 
-    if (*sem < 0) {  /* If the semaphore is negative, block the process */
+    /* BIG PICTURE:
+    - If the value of semaphore address is negative -> the resources are not available.
+    - Block the process first, then move it to ASL.
+    - Switch to a new process that needs less resources to execute by calling Scheduler().
+    */
+    if (*sem < 0) {  
         insertBlocked(sem, currProc);
         switchProcess();  /* Call the scheduler to run another process */
+    }
+}
+
+void signalOp(int *sem) {
+    pcb_PTR p;
+    (*sem) ++;
+
+    /* BIG PICTURE:
+    - If the value of semaphore address is positive -> there is free resource to allocate for a blocked process.
+    - Remove the first blocked process from ASL if there are any.
+    - If there exists a process, add it to ReadyQueue to proceed.
+    */
+    if (*sem >= 0) {
+        p = removeBlocked(sem);
+        if (p != NULL) {
+            insertProcQ(&ReadyQueue,p);
+        }
     }
 }

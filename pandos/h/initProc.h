98b0eef6aa2349f972b3738557f5c1d4dd841a28
Nicolas Nguyen <quangnguyen2003006@gmail.com>
2025-03-06 01:52:26 -0500

    add folders and implementation files for Phase 3

---
+++ pandos/h/initProc.h
@@ -0,0 +1,10 @@
+#ifndef INITPROC
+#define INITPROC
+
+/**************************************************************************** 
+ * Nicolas & Tran
+ * Declaration File for initProc.c module
+ * 
+ ****************************************************************************/
+#include "../h/types.h"
+#include "../h/const.h"

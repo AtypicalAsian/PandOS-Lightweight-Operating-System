d1a94b2f5ea63957e885520e4d9a2c5528ece821
Nicolas Nguyen <quangnguyen2003006@gmail.com>
2025-04-07 04:20:15 -0400

    make changes to branch to test

--- pandos/h/vmSupport.h
+++ pandos/h/vmSupport.h
@@ -13,7 +13,7 @@ void init_swap_structs();
 void uTLB_RefillHandler();
 void nuke_til_it_glows(int *semaphore);
 void reset_swap(int asid);
-void update_tlb_hanlder(pte_entry_t *new_page_table_entry);
+void update_tlb_handler(pte_entry_t *new_page_table_entry);
 int find_frame_swapPool();
 void flash_read_write(unsigned int read_write_bool, unsigned int device_no, unsigned int occp_pageNo, int swap_pool_index);
 void occupied_handler(unsigned int asid, unsigned int occp_pageNo, unsigned int swap_pool_index);

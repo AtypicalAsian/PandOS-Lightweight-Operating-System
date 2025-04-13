58342bbdc0db075879ed011f745b47214f8b9227
Nicolas Nguyen <quangnguyen2003006@gmail.com>
2025-04-13 14:38:11 -0400

    gen exception handler

--- pandos/h/sysSupport.h
+++ pandos/h/sysSupport.h
@@ -14,7 +14,7 @@ extern int support_device_sems[DEVICE_TYPES * DEVICE_INSTANCES];
 void returnControlSup(support_t *support, int exc_code);
 extern void sysSupportGenHandler();
 extern void trapExcHandler(support_t *currentSupport);
-void supportSyscallHandler(int exc_code, support_t *support_struct);
+void syscall_excp_handler(support_t *support_struct, int exceptionCode);
 void terminate(support_t *support_struct);
 void getTOD(state_PTR excState);
 void writeToPrinter(char *virtualAddr, int len, support_t *support_struct);
--- pandos/phase3/sysSupport.c
+++ pandos/phase3/sysSupport.c
@@ -358,9 +358,8 @@ void trapExcHandler(support_t *support_struct)
  *      4. Execute appropriate syscall handler
  *      5. LDST to return to process that requested SYSCALL
  **************************************************************************************************/
-void supportSyscallHandler(int exc_code, support_t *support_struct)
-{
-    if (exc_code < TERMINATE || exc_code > DELAY) {
+void syscall_excp_handler(support_t *support_struct, int exceptionCode){
+    if (exc_code < 9 || exc_code > 13) { /*Have to change for later phases right now we're just looking at syscall 9-13*/
         trapExcHandler(support_struct);
         return;
     }
@@ -408,15 +407,22 @@ void supportSyscallHandler(int exc_code, support_t *support_struct)
  *      3. Pass control to either the support level syscall handler or the program trap handler
  **************************************************************************************************/
 void sysSupportGenHandler() {
+    /*--------------Declare local variables---------------------*/
+    support_t* currProc_supp_struct;    /*current process support structure*/
+    int exception_code;                 /*exception code inside Cause Register*/
+    int requested_syscall_num;          /*syscall number requested by calling process*/
+    /*----------------------------------------------------------*/
 
-    support_t *support_struct = (support_t *) SYSCALL(GETSUPPORTPTR, 0, 0, 0);
-    int cause = support_struct->sup_exceptState[GENERALEXCEPT].s_cause;
-    int exc_code = EXCCODE(cause);
-    if (exc_code == 8) {
-        int syscall_num = support_struct->sup_exceptState[GENERALEXCEPT].s_a0;
-        supportSyscallHandler(syscall_num, support_struct);
+    /*Step 1: Obtain current process support structure via syscall number 8*/
+    currProc_supp_struct = (support_t*) SYSCALL(SYS8,0,0,0);
+
+    /*Step 2: Examine Cause register in exceptState field of support structure and extract exception code*/
+    exception_code = (((currProc_supp_struct->sup_exceptState[GENERALEXCEPT].s_cause) & GETEXCPCODE) >> CAUSESHIFT);
+
+    /*Step 3: Pass control to appropriate handler based on exception code*/
+    if (exception_code == SYSCONST){   /*If Cause.ExcCode is set to 8 -> call the syscall handler method [pandOS 3.7.1]*/
+        requested_syscall_num = currProc_supp_struct->sup_exceptState[GENERALEXCEPT].s_a0; /*the syscall number is stored in register a0 [pandOS 3.7.1]*/
+        syscall_excp_handler(currProc_supp_struct,requested_syscall_num); /*Pass to support level syscall handler*/
     }
-    else {
-        trapExcHandler(support_struct);
-    }
+    trapExcHandler(currProc_supp_struct); /*Otherwise, treat the exception as a program trap*/
 }

6eccb5f4b5ae6f693e5c276933a5e36de70e2885
Nicolas Nguyen <quangnguyen2003006@gmail.com>
2025-04-19 18:48:17 -0400

    add declarations to header file

--- pandos/h/deviceSupportDMA.h
+++ pandos/h/deviceSupportDMA.h
@@ -3,3 +3,18 @@
  * Declaration File for deviceSupportDMA.c module
  * 
  ****************************************************************************/
+#ifndef DEVICESUPPORTDMA
+#define DEVICESUPPORTDMA
+
+
+#include "../h/types.h"
+#include "../h/const.h"
+
+void disk_put(); /*sys14*/
+void disk_get(); /*sys15*/
+void flash_put(); /*sys16*/
+void flash_get(); /*sys17*/
+ 
+
+
+#endif

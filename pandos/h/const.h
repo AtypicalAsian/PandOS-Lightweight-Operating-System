e216ef124689986dea994320e3b799bcbff2fc3c
kaia15 <baotranongtran@gmail.com>
2025-04-19 13:34:04 -0400

    add more void(s) in ~/phase4/deviceSupportDMA.c

--- pandos/h/const.h
+++ pandos/h/const.h
@@ -106,48 +106,12 @@
 #define	LINE6			6				/* constant representing line 6 */
 #define	LINE7			7				/* constant representing line 7 */
 
-<<<<<<< HEAD
-/* Constant that represents when the first four bits in a terminal device's device register's status field are turned on */
-#define	TERM_DEV_STATUSFIELD_ON		0x0F
-
-/* device register field number for non-terminal devices */
-#define STATUS			  0
-#define COMMAND			  1
-#define DATA0			  2
-#define DATA1			  3
-
-/* device register field number for terminal devices */
-#define RECVSTATUS  	0
-#define RECVCOMMAND 	1
-#define TRANSTATUS  	2
-#define TRANCOMMAND 	3
-
-/* device common STATUS codes */
-#define UNINSTALLED		0
-#define READY			    1
-#define BUSY			    3
-#define PRINTCHR            2
-
-/*Printer Device*/
-#define PRINTER_LINE_NUM    6
-
-/* device common COMMAND codes */
-#define RESET			    0
-#define ACK				    1
-#define PRINTCHR            2 /* For printer device, specifically */
-
-/* Printer device */
-#define PRINTER_LINE_NUM    6
-#define PRINTER_READY       1
-#define PRINTER_BUSY        3
-=======
 /* Interupts constants */
 #define GETIP   0x0000FE00
 #define IPSHIFT 8
 #define TRANS_CHAR 5
 #define RECVD_CHAR 5
 #define TERMSTATUSMASK 0x000000FF
->>>>>>> fafd3b43fb303831ee72b8e7787e119e265fbed2
 
 
 /* syscall */
@@ -272,161 +236,22 @@
 #define BLOCK_SHIFT 8
 
 
-<<<<<<< HEAD
-/*Macro to get the index of the deviceSema4s array*/
-/*Shift each line's devices into 8-slot region. If it's a terminal device (line_no 7), have to see whether it's transimission or receive*/
-/*Otherwise, all other lines just use device 0-7*/
-#define GET_DEV_INDEX(dev_num,line_no,isReceive)\
-    (((line_no - 3) * 8) + ((line_no == 7) ? ((dev_num) * 2 + (isReceive)) : (dev_num)))
-
-
-
-#define CONST1  1
-#define CONST3  3
-
-#define MODEXCEPTION 1
-
-
-/* Phase 1: Additional constants */
-#define MAXPROC 20
-#define MAXPROC_SEM 22
-#define SMALLEST_ADDR 0x00000000
-#define LARGEST_ADDR  0x0FFFFFFF
-
-/* Phase 2: Additional constants */
-
-/* Related to the Pseudo-Clock: This is the max number of external (sub)devices in UMPS3, plus one additional semaphore */
-#define MAXDEVICECNT	49
-
-/*Address of top of the Nucleus stack page. Stack will start at this address and expand downward as data is pushed onto it*/
-#define TOPSTKPAGE   0x20001000
-
-/*interval timer intialized to 100ms in main() of initial.c*/
-#define INITTIMER       100000
-
-/*Pseudo-clock index in the device semaphores list*/
-#define PSEUDOCLOCKIDX  (MAXDEVICECNT-1)
-
-#define PSEUDOCLOCKSEM4INIT 0
-
-/*Initial values for process count, softBlockCount, device semaphores, accumulated time for a process that has been created*/
-#define INITSBLOCKCNT   0   /*initial soft block count*/
-#define INITPROCCNT     0   /*initial process count*/
-#define INITACCTIME     0   /*initial accumulated time for a process that has been created*/
-#define INITDEVICESEM   0   /*initial value for device semaphore*/
-
-#define SCHED_TIME_SLICE 5000  /*time slice for scheduler set to default 5ms*/
-#define FIRSTDEVIDX 0
-
-#define WORDSIZE     4
-
-
-/*Phase 3 addresses*/
-#define TEXT_START    0x800000B0
-#define SP_START      0xC0000000
-
-#define STACKSIZE   499
-#define ID_SHIFT    6
-
-/* Status Register Bitmask Constants (Processor State) */
-#define STATUS_ALL_OFF   0x0  /* Clears all bits in the Status register (useful for initializing or bitwise-OR operations) */
-#define STATUS_IE_ENABLE 0x00000004  /* Enables global interrupts (IEp, bit 2 = 1) after LDST */
-#define STATUS_PLT_ON    0x08000000  /* Enables the Processor Local Timer (PLT) (TE, bit 27 = 1) */
-#define STATUS_INT_ON    0x0000FF00  /* Enables all external interrupts by setting the Interrupt Mask bits */
-#define STATUS_USERPON	 0x00000008	/* constant for setting the user-mode on after LDST (i.e., KUp (bit 3) = 1) */
-#define	STATUS_IECOFF	 0xFFFFFFFE	/* constant for disabling the global interrupt bit (i.e., IEc (bit 0) = 0) */
-#define STATUS_IECON	 0x00000001	/* constant for enabling the global interrupt bit (i.e., IEc (bit 0) = 1) */
-
-#define INT_OFF     getSTATUS() & (STATUS_IECOFF)
-#define INT_ON      getSTATUS() | STATUS_IECON | STATUS_INT_ON
-
-#define	RESINSTRCODE	 0xFFFFFF28
-
-
-#define STATUS_KUc_SHIFT  1  /* KUc bit is bit 1 in the Status register */
-#define STATUS_KUc_MASK   0x1  /* Mask for extracting the bit (0000...0001) */
-#define USER_MODE         0x1  /* Value when in user mode */
-#define KERNEL_MODE       0x0  /* Value when in kernel mode */
-
-
-#define SYS1              1
-#define SYS2              2
-#define SYS3              3
-#define SYS4              4
-#define SYS5              5
-#define SYS6              6
-#define SYS7              7
-#define SYS8              8
-#define SYS9              9
-#define SYS10             10
-#define SYS11             11
-#define SYS12             12
-#define SYS13             13
-
-/* Constants for returning values in v0 to the caller */
-#define ERRCONST		-1			/* constant denoting an error occurred in the caller's request */
-#define SUCCESS	        0			/* constant denoting that the caller's request completed successfully */
-
-/**/
-#define LARGETIME        0xFFFFFFFF
-#define INITIAL_TIME     0
-
-#define NULL_PTR_ERROR   -1
-#define NO_INTERRUPTS    -1
-#define INTERRUPT_BITMASK_INITIAL  1
-
-#define SEM4BLOCKED     0
-#define OFFSET          3
-#define MAXSHAREIODEVS  48
-
-#define INDEXCLOCK      (MAXDEVICECNT - 1)
-
-#define MAXUPROCESS     8 /*max number of user processes can be run at any point in time*/
-#define MAXFRAMES       (MAXUPROCESS * 2)
-
-#define VALIDBITOFF     0xFFFFFDFF
-#define V_BIT_SET       0x00000200      /*Bit 9*/
-#define D_BIT_SET       0x00000400      /*Bit 10*/
-
-#define FREEFRAME       -1
-
-#define LINENUM7        7
-
-/*Cause Register Constansts (used in gen_exception_handler in initial.c)*/
-#define CAUSESHIFT      2       /**/
-#define GETEXCPCODE     0x0000007C /*set Cause register bits to 0 except for ExcCode field*/
-#define INTCONST		0			/* exception code signaling an interrupt occurred */
-#define TLBCONST		3			/* upper bound on the exception codes that signal a TLB exception occurred */
-#define SYSCONST		8			/* exception code signaling a SYSCALL occurred */
-
-
-/* operations */
-#define	MIN(A,B)		((A) < (B) ? A : B)
-#define MAX(A,B)		((A) < (B) ? B : A)
-#define	ALIGNED(A)		(((unsigned)A & 0x3) == 0)
-
-/* Terminal Device Codes */
-#define TERMINAL_LINE_NUM   7
-
-#define TERMINAL_STATUS_NOT_INSTALLED   0
-#define TERMINAL_STATUS_READY   1
-#define TERMINAL_STATUS_TRANSMITTED 5
-#define TERMINAL_STATUS_RECEIVED    5
-
-#define TERMINAL_COMMAND_TRANSMITCHAR   2
-
-#define TERMINAL_CHAR_SHIFT 8
-#define TERMINAL_STATUS_MASK    0xFF
-
-/* Macro to load the Interval Timer */
-=======
 #define EXCSTATE ((state_t *) BIOSDATAPAGE)
->>>>>>> fafd3b43fb303831ee72b8e7787e119e265fbed2
 #define LDIT(T)	((* ((cpu_t *) INTERVALTMR)) = (T) * (* ((cpu_t *) TIMESCALEADDR))) 
 #define STCK(T) ((T) = ((* ((cpu_t *) TODLOADDR)) / (* ((cpu_t *) TIMESCALEADDR))))
 #define IP(C) ((C & 0x0000FF00) >> 8)
 
-
+/* Phase 4 Constants*/
+#define HEADADDRSHIFT 8
+#define CYLADDRSHIFT 16
+#define RESETACKSHIFT 24
+#define LOWERMASK 0xFF
+#define DISK_RESET 0
+#define DISK_ACK 1
+#define SEEKCYL 2
+#define READBLK 3
+#define WRITEBLK 4
+#define DISKREADY 1
 
 #endif
 
--- pandos/phase4/deviceSupportDMA.c
+++ pandos/phase4/deviceSupportDMA.c
@@ -8,3 +8,83 @@
  * Nicolas & Tran  
  * View version history and changes: https://github.com/AtypicalAsian/CS372-OS-Project
  **************************************************************************************************/
+
+#include "../h/types.h"
+#include "../h/const.h"
+#include "../h/asl.h"
+#include "../h/pcb.h"
+#include "../h/initial.h"
+#include "../h/scheduler.h"
+#include "../h/exceptions.h"
+#include "../h/interrupts.h"
+#include "../h/initProc.h"
+#include "../h/vmSupport.h"
+#include "../h/sysSupport.h"
+#include "/usr/include/umps3/umps/libumps.h"
+
+int devSema4_support[DEVICE_TYPES * DEV_UNITS]; 
+
+hidden void disk_put(int *logicalAddr, int diskNo, int sectNo, support_t *support_struct);
+hidden void disk_get(int *logicalAddr, int diskNo, int sectNo, support_t *support_struct);
+hidden void flash_put(int *logicalAddr, int flashNo, int blockNo, support_t *support_struct);
+hidden void flash_get(int *logicalAddr, int flashNo, int blockNo, support_t *support_struct);
+
+/*
+ * Ref: princOfOperations - section 5.3, pandOS - section 5.2.1, 5.1 (fig 5.1)
+ */
+void disk_put (int *logicalAddr, int diskNo, int sectNo, support_t *support_struct) {
+
+    /* Find the disk device register with given diskNo */
+    devregarea_t *busRegArea = (devregarea_t *) RAMBASEADDR;
+    device_t d_device = &busRegArea->devreg[diskNo];
+
+    int maxCyl = d_device->d_data1 >> CYLADDRSHIFT;
+    int maxHd = (d_device->d_data1 >> HEADADDRSHIFT) & LOWERMASK;
+    int maxSect = d_device->d_data1 & LOWERMASK;
+
+    /* Validate the sector address, where we perform WRITE operation into 
+     * if it's not outside of U's proc logical address 
+     */
+    if (sectNo < 0 || sectNo >= (maxCyl * maxHd * maxSect)) {
+        SYSCALL(SYS9, 0, 0, 0);
+        return;
+    }
+
+    int cyl = sectNo / (maxHd * maxSect);
+    int temp = sectNo % (maxHd * maxSect);
+    int hd = temp / maxSect;
+    int sect = temp % maxSect;
+
+    setSTATUS(NO_INTS);
+
+    disk->d_command = (cyl << HEADADDRSHIFT) | SEEKCYL;
+    /* Issue I/O Request to suspend current U's proc until the disk WRITE/READ operation is done */
+    int status = SYSCALL(SYS5, DISKINT, diskNo, 0);
+
+    /* If the operation ends with a status other than “Device Ready”
+     * (1), the negative of the completion status is returned in v0*/
+    if (status != DISKREADY) {
+        supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = -status;
+        setSTATUS(YES_INTS);
+        return;
+    }
+
+    /* Set DMA address for WRITE operation: "A disk drive DATA0 device register field is 
+     * read/writable and is used to specify the starting physical address 
+     * for a read or write DMA operation" 
+     */
+    disk->d_data0 = (memaddr)logicalAddr;
+    disk->d_command = (hd << RESETACKSHIFT) | (sect << CYLADDRSHIFT) | WRITEBLK;
+
+    /* Issue I/O Request again to check whether WRITE operation ends with SUCCESS status */
+    status = SYSCALL(SYS5, DISKINT, diskNo, 0);
+    if (status != DISKREADY) {
+        supportStruct->sup_exceptState[GENERALEXCEPT].s_v0 = -status;
+        setSTATUS(YES_INTS);
+        return;
+    }
+
+    disk->d_command = DISK_ACK;
+    setSTATUS(YES_INTS);
+    support_struct->sup_exceptState[GENERALEXCEPT].s_v0 = DISKREADY;
+}

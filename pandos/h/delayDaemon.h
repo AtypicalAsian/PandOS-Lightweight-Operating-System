d66e86076bf4918473caf49a7955bfb0ce1a373a
Nicolas Nguyen <quangnguyen2003006@gmail.com>
2025-04-26 23:44:06 -0400

    phase 5 all functions to start testing

--- pandos/h/delayDaemon.h
+++ pandos/h/delayDaemon.h
@@ -13,9 +13,10 @@ void initADL(); /*initialize the Active Delay List (ADL)*/
 void sys18Handler(int sleepTime, support_t *support_struct); /*function to implement syscall 18*/
 void delayDaemon(support_t *currSuppStruct); /*code for delay daemon process*/
 
-void alloc_descriptor(); /*allocate new node for the ADL*/
-void remove_descriptor(); /*return a node from the ADL to the free pool (of unsued descriptor nodes)*/
+delayd_PTR alloc_descriptor(); /*allocate new node for the ADL*/
+void return_to_ADL(delayd_PTR delayDescriptor); /*return a node from the ADL to the free pool (of unsued descriptor nodes)*/
 int insertADL(); /*insert new descriptor into Active Delay List (ADL)*/
 int removeADL(); /*remove descriptor from Active Delay List (ADL)*/
+delayd_PTR searchADL(int wakeTime);
 
 #endif

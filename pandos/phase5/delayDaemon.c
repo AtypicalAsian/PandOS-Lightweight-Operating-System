6cb9f0208aec42a4de063a31d255b383fcc40065
Nicolas Nguyen <quangnguyen2003006@gmail.com>
2025-05-04 16:58:45 -0400

    test with removeADL again

--- pandos/phase5/delayDaemon.c
+++ pandos/phase5/delayDaemon.c
@@ -186,7 +186,7 @@ int insertADL(int time_asleep, support_t *supStruct){
  **************************************************************************************************/
 void removeADL(int currTime){
     delayd_PTR currNode = delayd_h->d_next; /*get head of ADL*/
-    while ((currNode->d_wakeTime <= currTime) && (currNode != delayd_tail)){
+    while ((currNode != delayd_tail) && (currNode->d_wakeTime <= currTime) ){
         /*if (currNode->d_wakeTime < 0){
             get_nuked(NULL);
         }*/
@@ -214,12 +214,7 @@ void delayDaemon(){
         SYSCALL(SYS3,(int) &delayDaemon_sema4,0,0);
         STCK(curr_time);
         delayd_PTR curr = delayd_h->d_next;
-        while (curr != delayd_tail && curr->d_wakeTime <= curr_time){
-            SYSCALL(SYS4,(int)&curr->d_supStruct->privateSema4,0,0);
-            delayd_h->d_next = curr->d_next;
-            free_descriptor(curr);
-            curr = delayd_h->d_next;
-        }
+        removeADL(curr_time);
         SYSCALL(SYS4,(int)&delayDaemon_sema4,0,0);
     }
 }
